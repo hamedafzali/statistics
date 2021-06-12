0ef0dd3af07c809a7919b318771fa71e4d24e7e5
hamed afzali <AFZALI.HAMED@GMAIL.COM>
2021-06-12 14:39:22 +0430

    maintain some codes

--- src/services/password.js
+++ src/services/password.js
@@ -1,17 +1,32 @@
 import http from "./httpService";
 import { apiUrl } from "../config.json";
 
+// export function changePassword(nationalCode, oldPassword, newPassword) {
+//   //console.log("personlocationsetstatus", nationalcode);
+//   return http.get(
+//     apiUrl +
+//       `/auth/changepassword/${nationalCode}/${oldPassword}/${newPassword}`
+//   );
+// }
+
 export function changePassword(nationalCode, oldPassword, newPassword) {
   //console.log("personlocationsetstatus", nationalcode);
-  return http.get(
-    apiUrl +
-      `/auth/changepassword/${nationalCode}/${oldPassword}/${newPassword}`
-  );
+  return http.post(apiUrl + `/auth/changepassword`, {
+    nationalcode: nationalCode,
+    oldpassword: oldPassword,
+    newpassword: newPassword,
+  });
 }
+// export function resetPassword(nationalCode, newPassword) {
+//   //console.log("personlocationsetstatus", nationalcode);
+//   return http.get(
+//     apiUrl + `/auth/resetpassword/${nationalCode}/${newPassword}`
+//   );
+// }
 
 export function resetPassword(nationalCode, newPassword) {
-  //console.log("personlocationsetstatus", nationalcode);
-  return http.get(
-    apiUrl + `/auth/resetpassword/${nationalCode}/${newPassword}`
-  );
+  return http.post(apiUrl + `/auth/resetpassword`, {
+    nationalcode: nationalCode,
+    newpassword: newPassword,
+  });
 }
